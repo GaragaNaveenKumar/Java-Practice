94595dafb3678ba0d5a9e508d1ab189c76808ca5
GaragaNaveenKumar <23a35a0517@pragati.ac.in>
2026-02-09 21:32:18 +0530

    basics

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+    public static void main(String[] args) {
+        int x=7;
+        int y=5;
+        boolean res=x<y;
+        System.out.println(res);
+        int i=1;
+        while(i<=4){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+    public static void main(String args[]){
+        int num1=3;
+        int num2=5;
+        System.out.print(num1+num2);
+    }
+}
